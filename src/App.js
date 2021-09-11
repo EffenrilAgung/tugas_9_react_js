3868d6c895ffd73ddce71f1e8d24011506a58fb6
EffenrilAgung <effenrilagung300@gmail.com>
2021-09-12 00:08:38 +0700

    Tugas_9_react_js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Berhasil</h1>
+      </div>
+    )
+  } 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
+}
+export default App
